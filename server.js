efeb144922c92e39c6ec610e3576def6c47f5109
Marc Dacanay <mdacanay@gmail.com>
2020-04-21 21:47:56 +1000

    Implement Query with args

--- server.js
+++ server.js
@@ -1,26 +1,35 @@
-var express = require('express');
-var graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
+var express = require("express");
+var graphqlHTTP = require("express-graphql");
+var { buildSchema } = require("graphql");
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
-  type Query {
-    hello: String
-  }
+    type Query {
+      rollDice(numDice: Int!, numSides: Int): [Int]
+    }
 `);
 
 // The root provides a resolver function for each API endpoint
 var root = {
-  hello: () => {
-    return 'Hello world!';
-  },
+    rollDice: rollDice,
 };
 
+function rollDice(args) {
+    var output = [];
+    for (var i = 0; i < args.numDice; i++) {
+        output.push(1 + Math.floor(Math.random() * (args.numSides || 6)));
+    }
+    return output;
+}
+
 var app = express();
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true,
-}));
+app.use(
+    "/graphql",
+    graphqlHTTP({
+        schema: schema,
+        rootValue: root,
+        graphiql: true,
+    })
+);
 app.listen(4000);
-console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+console.log("Running a GraphQL API server at localhost:4000/graphql");
